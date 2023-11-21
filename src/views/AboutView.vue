a9e8e6dc08f877f2854ba84eddb5a1518f9e22c3
Lin-c-c <linchanghe2023@163.com>
2023-11-21 17:50:58 +0800

    写了两个按钮

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,37 @@
+<script>
+export default {
+  // reactive state
+  data() {
+    return {
+      count1: 0,
+      count2: 99, //别一直戳会变成负数的qwq
+      count3: 66
+    }
+  },
+
+  // functions that mutate state and trigger updates
+  methods: {
+    increment() {
+      this.count1++; this.count2--
+    },
+    decrease() {
+      this.count3--
+    }
+  },
+
+  // lifecycle hooks
+  mounted() {
+    console.log(`The initial count is ${this.count1}.`)
+    console.log(`The initial count is ${this.count2}.`)
+    console.log(`The initial count is ${this.count3}.`)
+  }
+}
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="increment">王老师戳了我{{ count1 }}下，剩余{{ count2 }}下</button>
+    <button @click="decrease">这个点一下会-1:{{ count3 }}</button>
   </div>
 </template>
 
